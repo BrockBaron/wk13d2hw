908bacdfbcf6f784158322d7bfbb4d8d3d93e3d5
BrockBaron <Nslekkas@gmail.com>
2021-06-01 20:26:44 +0100

    inital commit

---
+++ src/main/java/com/hw/example/wk13d2hw/Wk13d2hwApplication.java
@@ -0,0 +1,13 @@
+package com.hw.example.wk13d2hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wk13d2hwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wk13d2hwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hw/example/wk13d2hw/Wk13d2hwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.example.wk13d2hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wk13d2hwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
