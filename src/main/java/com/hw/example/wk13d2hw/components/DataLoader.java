8d26196e94488f72e7ffbf6950d221c60bfb5b2a
BrockBaron <Nslekkas@gmail.com>
2021-06-01 22:34:54 +0100

    Data Loader started

--- src/main/java/com/hw/example/wk13d2hw/components/DataLoader.java
+++ src/main/java/com/hw/example/wk13d2hw/components/DataLoader.java
@@ -1,4 +1,38 @@
 package com.hw.example.wk13d2hw.components;
 
-public class DataLoader {
+
+import com.hw.example.wk13d2hw.repositories.FileRepository;
+import com.hw.example.wk13d2hw.repositories.FolderRepository;
+import com.hw.example.wk13d2hw.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    FileRepository fileRepository;
+
+    public void run(ApplicationArguments args) {
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
